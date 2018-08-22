cee8165af5cea1cbb3b0a7b5e8b47e6fe318c2ae
Thiago Silva Pereira <thiagoufma@gmail.com>
2018-08-22 00:19:02 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,42 +1,39 @@
 <?php 
-
-	require_once("vendor/autoload.php");
-
-	use \Slim\Slim;
-	use \Hcode\Page;
-	use \Hcode\PageAdmin;
-	use \Hcode\Model\User;
-	
-	$app = new Slim();
-	$app->config('debug', true);
-	
-	$app->get('/', function() {
+session_start();
+require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+$app = new Slim();
+$app->config('debug', true);
+$app->get('/', function() {
     
-		$page = new Page();
-		$page->setTpl("index");
-	});
-
-	$app->get('/admin', function() {
+	$page = new Page();
+	$page->setTpl("index");
+});
+$app->get('/admin', function() {
     
-		$page = new PageAdmin();
-		$page->setTpl("index");
-	});
-	
-	$app->get('/admin/login', function() {
-    
-		$page = new PageAdmin([
-			"header" =>false,
-			"footer" =>false
-		]);
-		$page->setTpl("login");
-	});
-
-	$app->post('/admin/login', function(){
-
-		User::login($_POST["login"], $_POST["password"]);
-		header("Location: /admin");
-		exit;
-	});
-
-	$app->run();
-?>
+	User::verifyLogin();
+	$page = new PageAdmin();
+	$page->setTpl("index");
+});
+$app->get('/admin/login', function() {
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("login");
+});
+$app->post('/admin/login', function() {
+	User::login($_POST["login"], $_POST["password"]);
+	header("Location: /admin");
+	exit;
+});
+$app->get('/admin/logout', function() {
+	User::logout();
+	header("Location: /admin/login");
+	exit;
+});
+$app->run();
+ ?>
